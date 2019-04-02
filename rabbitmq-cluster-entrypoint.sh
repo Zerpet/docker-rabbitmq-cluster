395a0c20b6239ee6d1ca7e8120ef2e90aa229667
Aitor Cedres <acedres@pivotal.io>
2019-04-02 16:49:19 +0100

    Update path to docker-entrypoint
    The parent image changed the location of the docker-entrypoint script
    location. Updated the paths to point to the new location.

--- rabbitmq-cluster-entrypoint.sh
+++ rabbitmq-cluster-entrypoint.sh
@@ -9,7 +9,7 @@ fi
 echo "Starting RabbitMQ Node ${NODENAME}@$(hostname)"
 
 if [ -z "$CLUSTER_WITH" -o "$CLUSTER_WITH" = "$(hostname)" ]; then
-    /docker-entrypoint.sh rabbitmq-server
+    /usr/local/bin/docker-entrypoint.sh rabbitmq-server
 else
     echo "Setup Cluster with $CLUSTER_WITH for node ${NODENAME}"
     rabbitmqctl wait /var/lib/rabbitmq/mnesia/${NODENAME}@$(hostname).pid && sleep 20 && \
@@ -17,6 +17,6 @@ else
         rabbitmqctl stop_app && \
         rabbitmqctl join_cluster ${ENABLE_RAM:+--ram} ${CLUSTER_WITH} && \
         rabbitmqctl start_app &
-    /docker-entrypoint.sh rabbitmq-server
+    /usr/local/bin/docker-entrypoint.sh rabbitmq-server
 fi
 
